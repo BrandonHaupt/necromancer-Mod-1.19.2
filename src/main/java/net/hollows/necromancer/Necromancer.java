d83af9926e840e55cc3f86f107048016010addd2
Brandon Haupt <haupt.brandon@gmail.com>
2022-09-11 09:26:27 -0500

    Initial commit

---
+++ src/main/java/net/hollows/necromancer/Necromancer.java
@@ -0,0 +1,16 @@
+package net.hollows.necromancer;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Necromancer implements ModInitializer {
+	public static final String MOD_ID = "necromancer";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/hollows/necromancer/NecromancerClient.java
@@ -0,0 +1,10 @@
+package net.hollows.necromancer;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NecromancerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
